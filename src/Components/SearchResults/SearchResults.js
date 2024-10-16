c55511078a9d7bb3964699e2fec5650841378599
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-10-16 18:08:44 +0300

    hardcoded artists in App.js  and in SerarchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -6,6 +6,15 @@ class  SearchResults extends React.Component {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
+                <ul>
+                    {this.props.results.map(result => (
+                        <li key={result.id}>
+                            <p>Name: {result.name}</p>
+                            <p>Artist: {result.artist}</p>
+                            <p>Album: {result.album}</p>
+                        </li>
+                    )) }
+                </ul>   
                 {/* <TrackList /> */}
             </div>
         )
