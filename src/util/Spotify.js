f3514b3d132fd74e3fe0fd8f9ad4b0bb7e81e1b7
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-11-04 01:11:17 +0200

    started with Spodify.js

---
+++ src/util/Spotify.js
@@ -0,0 +1,9 @@
+let getAccessToken;  /* 1:32:18 */
+const spodify = {
+    get getAccessToken() {
+        if (getAccessToken){
+            return getAccessToken
+        }
+    }
+}
+export default spodify;
